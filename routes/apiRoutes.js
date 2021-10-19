31aa83f8dc61e0bbbee2290710ad16bb9ac0332d
Steven Sosebee <swsosebee@gmail.com>
2021-10-19 12:27:16 -0400

    finalized routes and styling.  Updated README documentation

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const notes = require('express').Router();
+const { clog } = require('../middleware/clog');
 const { v4: uuidv4 } = require('uuid');
 const {
   readFromFile,
@@ -12,4 +13,39 @@ notes.get('/', (req, res) => {
     .then((data) => res.json(JSON.parse(data)));
 });
 
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  console.info(`deleting ${noteId}`);
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+      // console.info(result);
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 module.exports = notes;
